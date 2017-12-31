d6ab34cac960583d5e0575798ecaf6702593bcfb
PRAVEEN KUMAR GUPTA <guptapraveen@gmail.com>
2017-12-31 17:04:02 +0530

    Added Features : Play Again Option Added.

--- src/game.js
+++ src/game.js
@@ -1,71 +1,92 @@
-
-const Game=function(topLeft,bottomRight) {
-  this.topLeft=topLeft;
-  this.bottomRight=bottomRight;
-  this.snake={};
-  this.food={};
+const Game = function(topLeft, bottomRight) {
+  this.topLeft = topLeft;
+  this.bottomRight = bottomRight;
+  this.snake = {};
+  this.food = {};
   this.score = 0;
 }
 
-Game.prototype.addScore = function(){
-  this.score += 10;
+Game.prototype.addScore = function() {
+    this.score += 10;
 };
 
-Game.prototype.updateScore = function (head) {
-  if(this.food.x == head.x && this.food.y == head.y){
+Game.prototype.updateScore = function(head) {
+  if (this.snake.head.isSameCoordAs(this.food.getPosition())) {
     this.addScore();
   };
 }
 
-Game.prototype.addSnake=function(snake) {
-  this.snake=snake;
+Game.prototype.addSnake = function(snake) {
+  this.snake = snake;
 }
 
-Game.prototype.getSnake=function() {
+Game.prototype.getSnake = function() {
   return snake;
 }
 
-Game.prototype.turnLeft=function() {
+Game.prototype.turnLeft = function() {
   return this.snake.turnLeft();
 }
 
-Game.prototype.turnRight=function() {
+Game.prototype.turnRight = function() {
   return this.snake.turnRight();
 }
 
-Game.prototype.grow=function() {
-  let growthFactor=this.food.getGrowthFactor();
-  console.log(growthFactor);
+Game.prototype.grow = function() {
+  let growthFactor = this.food.getGrowthFactor();
+  // console.log(growthFactor);
   return this.snake.grow(growthFactor);
 }
 
-Game.prototype.getFood=function() {
+Game.prototype.getFood = function() {
   return this.food;
 }
 
-Game.prototype.move=function() {
-  let details={};
-  details.oldHead=this.snake.getHead();
-  details.oldTail=this.snake.move();
-  details.head=this.snake.getHead();
-  console.log(details.head);
+Game.prototype.move = function() {
+  let details = {};
+  details.oldHead = this.snake.getHead();
+  details.oldTail = this.snake.move();
+  details.head = this.snake.getHead();
   return details;
 }
 
-Game.prototype.hasSnakeEatenFood=function() {
+Game.prototype.hasSnakeEatenFood = function() {
   return this.snake.head.isSameCoordAs(this.food.getPosition());
 }
 
-Game.prototype.createFood=function() {
+Game.prototype.createFood = function() {
   console.log(this.bottomRight);
-  let position=generateRandomPosition(this.bottomRight.x,this.bottomRight.y);
+  let position = generateRandomPosition(this.bottomRight.x, this.bottomRight.y);
 
-  let random=generateRandomNumberBetween(0,10);
-  let growthFactor=1;
-  let superFood=false;
-  if(random>5) {
-    growthFactor=10;
-    superFood=true;
+  let random = generateRandomNumberBetween(0, 10);
+  let growthFactor = 1;
+  let superFood = false;
+  if (random > 5) {
+    growthFactor = 10;
+    superFood = true;
   }
-  this.food=new Food(position,growthFactor,superFood);
+  this.food = new Food(position, growthFactor, superFood);
+}
+
+
+Game.prototype.isSnakeXAxisEdgePosition = function(xPosition) {
+  return xPosition == 0 || xPosition == numberOfCols - 1;
+};
+
+Game.prototype.isSnakeYAxisEdgePosition = function(yPosition) {
+  return yPosition == 0 || yPosition == numberOfRows - 1;
+}
+
+Game.prototype.isSnakeAtEdgePosition = function(xPosition, yPosition) {
+  return this.isSnakeXAxisEdgePosition(xPosition) || this.isSnakeYAxisEdgePosition(yPosition);
+}
+
+Game.prototype.checkSnakePositions = function(xPosition, yPosition) {
+  return this.isSnakeAtEdgePosition(xPosition, yPosition) || snake.isSnakeEatingItself();
+}
+
+Game.prototype.hasGameOver = function(snakeHead) {
+  let xPosition = snakeHead.x;
+  let yPosition = snakeHead.y;
+  return this.checkSnakePositions(xPosition, yPosition);
 }
--- src/main.js
+++ src/main.js
@@ -1,42 +1,18 @@
 let game=undefined;
-// let food=undefined;
 let numberOfRows=60;
 let numberOfCols=120;
 let animator=undefined;
 
-const stopGame = function (head) {
-  let xPosition = head.x;
-  let yPosition = head.y;
-  checkPositionsAndStopGame(xPosition,yPosition);
-}
-
-const isXAxisEdgePosition = function (xPosition) {
-  return xPosition == 0 || xPosition == numberOfCols - 1;
-};
-
-const isYAxisEdgePosition = function (yPosition) {
-  return yPosition == 0 || yPosition == numberOfRows - 1;
-
-}
-
-const isAnyEdgePosition = function(xPosition,yPosition){
-  return isXAxisEdgePosition(xPosition) || isYAxisEdgePosition (yPosition);
-}
-
-const checkPositionsAndStopGame = function(xPosition,yPosition){
-  if(isAnyEdgePosition(xPosition,yPosition)||snake.isSnakeEatingItself()){
-    clearInterval(animator);
-    document.getElementById("game-Over").innerText ="Game Over";
-  }
-};
-//
 const playAgain = function () {
-  document.getElementById("game-Over").innerText ="";
   location.reload();
 };
 
-const displayScore = function () {
-  document.getElementById("scoreUpdate").innerText = `Score : ${game.score}`;
+const stopGame = function (head) {
+  if(game.hasGameOver(head)){
+    clearInterval(animator);
+    document.getElementById("game-Over").innerText = "Game Over";
+    document.getElementById("restart-Button").innerHTML = `<button id= "restart-Button" onclick=playAgain()>Play Again</button>`
+  }
 };
 
 const animateSnake=function() {
@@ -49,7 +25,6 @@ const animateSnake=function() {
     game.grow();
     game.createFood();
     drawFood(game.getFood());
-    game.addScore();
     displayScore();
   }
   stopGame(details.head);
@@ -77,7 +52,6 @@ const addKeyListener=function() {
 }
 
 const createSnake=function() {
-  // game = new Game();
   let tail=new Position(12,10,"east");
   let body=[];
   body.push(tail);
